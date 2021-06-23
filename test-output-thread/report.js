55da436008f265b8c4d87ce435c0b6ecfe8f7345
bipinmnair89 <bipinmnair89@gmail.com>
2021-06-23 20:28:28 +0530

    Entire Cucumber-testng runner framework with Page object model,parallel execution and fail rerun functionalities

---
+++ test-output-thread/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "f86362b7-5994-4253-bc85-eddefe27e196",
+    "feature": "My Accounts page feature",
+    "scenario": "Dummy failure scenario test",
+    "start": 1624456908491,
+    "group": 12,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1624456941855,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 12,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  }
+]);
+});
